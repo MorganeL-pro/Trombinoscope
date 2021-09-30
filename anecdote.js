089fad86e579e99036757dbc9d24fa08777c8ebb
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2021-09-30 16:56:49 +0200

    add maj

--- anecdote.js
+++ anecdote.js
@@ -9,7 +9,9 @@ const anecdotes = [
     "    Le Web n'est pas l'Internet. Quand on parle de l'Internet il s'agit du réseau informatique qui permet de communiquer entre les ordinateurs. Le Web se sert de cette capacité de l'Internet à communiquer des informations pour le faire sous formes de pages et de liens au travers d'un navigateur.",
     "    1989, frontière franco-suisse, Conseil Européen pour la Recherche nucléaire-CERN. L’informaticien britannique Tim Berners-Lee discute avec son supérieur d’une propositions «vague mais excitante». Le web est né !",
     "    1993, Une imprimante aux origines de Linux. Richard Stallman travail au MIT. Une imprimante qui ne fonctionne pas très bien et il décide de corriger le driver. Petit problème pour Stallman, le code source du driver n'est pas fourni. Pour lui c'est la goutte d'eau. il lance le projet GNU (acronyme de GNU is Not Unix).",
-];
+    "    2005, les relations entre la communauté développant le noyau Linux et la société en charge du développement de BitKeeper furent rompues, et le statut de gratuité de l’outil fut révoqué. La communauté du développement de Linux (et plus particulièrement Linus Torvalds) a développer son propre outil en se basant sur l’utilisation de BitKeeper.",
+
+  ];
 
 //nombre aleatoire
 function entierAleatoire(min, max){
@@ -18,7 +20,7 @@ function entierAleatoire(min, max){
 
 // insert une valeur du tab anecdote sur la page html 
 function textAnecdote() {
-    let entier = entierAleatoire(0, 4);
+    let entier = entierAleatoire(0, 5);
     document.querySelector(".anecdote").innerHTML = anecdotes[entier];
   }
 
